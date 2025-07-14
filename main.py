ec6a0d75c2cdedeea833e5a0a10094d2e5aae7d7
benjasilvaa <162464013+benjasilvaa@users.noreply.github.com>
2025-07-14 07:48:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from parque import Parque
 if __name__ == "__main__":
     parque = Parque()
 
-    print("\n🚨 Creando visitantes automáticos (no se muestran)...")
+    print("\n Creando visitantes automáticos (no se muestran)...")
     parque.crear_visitantes_automaticos(10)
 
-    print("\n🧍 Ingresá visitantes manuales (se verán sus movimientos)...")
+    print("\n Ingresá visitantes manuales (se verán sus movimientos)...")
     parque.crear_visitantes_interactivo()
 
-    print("\n🎢 Iniciando simulación del parque...\n")
+    print("\n Iniciando simulación del parque...\n")
     parque.simular_parque()
